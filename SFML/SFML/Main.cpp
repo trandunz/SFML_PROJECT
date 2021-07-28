a153bdec75849bc91b8c0264ea67bdf6819c9628
trandunz <54924605+trandunz@users.noreply.github.com>
2021-07-28 23:23:09 +1200

    player

--- SFML/SFML/Main.cpp
+++ SFML/SFML/Main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
+#include "Player.h"
 
 namespace Utils
 {
@@ -16,38 +17,12 @@ int main()
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode(Utils::WINDOWWIDTH, Utils::WINDOWHEIGHT), "SFML works!", sf::Style::Default, settings);
+    sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(Utils::WINDOWWIDTH, Utils::WINDOWHEIGHT), "SFML works!", sf::Style::Default, settings);
     
 
-    sf::CircleShape circle(100.f); // circle
-    sf::RectangleShape square(sf::Vector2f(100, 100)); // square
+    Player pPlayer(window);
 
-    circle.setFillColor(sf::Color::Green);
-    circle.setPointCount(100);
     
-
-    square.setPosition(250, 250);
-    circle.setPosition(50, 50);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(circle);
-        window.draw(square);
-        window.display();
-    }
 
     return 0;
-}
- 
-void Render()
-{
-
 }
---
+++ SFML/SFML/Player.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SFML/graphics.hpp>
+class Player
+{
+public:
+	Player(sf::RenderWindow* _renderWindow);
+	~Player();
+	void Render();
+	void Start();
+	void Update();
+	sf::Vector2f Movement();
+	sf::View PlayerView();
+
+	void SetView(sf::View _view);
+
+private:
+	sf::RenderWindow* m_rRenderWindow;
+	sf::CircleShape m_oPlayerBody;
+	const float m_fFPS = 60.0f;
+
+	sf::Clock clClock;
+
+	bool redraw;
+
+};
+
