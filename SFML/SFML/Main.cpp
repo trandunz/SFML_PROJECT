15bf69417a54c93f62828d178f4ae7ca37946199
trandunz - school <54924605+trandunz@users.noreply.github.com>
2021-08-11 12:31:35 +1200

    +uPDATE+START+rENDER()

--- SFML/SFML/Main.cpp
+++ SFML/SFML/Main.cpp
@@ -7,47 +7,76 @@ namespace Utils
     int WINDOWWIDTH = 400;
 }
 
+void Start();
+void Update();
+void Render();
+
+sf::RenderWindow* m_RenderWindow;
 
 int main()
 {
+	//WINDOW HANDLE
     HWND hwnd = GetConsoleWindow();
-    ShowWindow(hwnd, SW_HIDE);
+    ShowWindow(hwnd, SW_SHOW);
 
+	//SETTINGS
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode(Utils::WINDOWWIDTH, Utils::WINDOWHEIGHT), "SFML works!", sf::Style::Default, settings);
+	//OBJECT CREATION
+	m_RenderWindow = new sf::RenderWindow(sf::VideoMode(Utils::WINDOWWIDTH, Utils::WINDOWHEIGHT), "SFML works!", sf::Style::Default, settings);
     
-
     sf::CircleShape circle(100.f); // circle
     sf::RectangleShape square(sf::Vector2f(100, 100)); // square
 
     circle.setFillColor(sf::Color::Green);
     circle.setPointCount(100);
-    
-
     square.setPosition(250, 250);
     circle.setPosition(50, 50);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	//
+	Start();
+	Update();
 
-        window.clear();
-        window.draw(circle);
-        window.draw(square);
-        window.display();
-    }
+	// CLEAN UP
+	delete m_RenderWindow;
+	m_RenderWindow = nullptr;
 
+	//RETURN NULL FOR MAIN
     return 0;
+}
+
+void Start() 
+{
+
+}
+
+void Update()
+{
+	//MAIN GAME LOOP
+	while (m_RenderWindow->isOpen())
+	{
+		sf::Event event;
+		while (m_RenderWindow->pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				m_RenderWindow->close();
+		}
+		// Object Updates
+
+		//Render
+		Render();
+		
+	}
 }
  
 void Render()
 {
+	m_RenderWindow->clear();
 
+	// Draw Objects
+	/*m_RenderWindow->draw(circle);
+	m_RenderWindow->draw(square);*/
+
+	m_RenderWindow->display();
 }
